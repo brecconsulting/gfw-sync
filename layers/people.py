e41063f41a3842fe7baf5559d9c1c081cef22dd9
astrong19 <astrong@wri.org>
2014-10-10 17:23:08 -0400

    Land Rights Datasets Added

--- layers/people.py
+++ layers/people.py
@@ -14,27 +14,79 @@ def layers():
 
 
 
-    can_people = {
-        'location': "geojson_url",
-        'full_path': "http://data.gfw.opendata.arcgis.com/datasets/5c3ce80d80df4d7f9f6a39a19f15b90d_0.geojson",
-        'where_clause': "\"Status\" <> 'CANCELLED'", #make sure to escape quotes (\")
+    Canada_Land_Rights = {
+        'location': "server",
+        'full_path': "D:\\GIS Data\\CAN\\Canada Land Rights\\Canada_Land_Rights.shp",
+        'where_clause': "",
+        'transformation': "NAD_1983_To_WGS_1984_33",
+        'fields': {
+            'Name': ["field", "NAME"],
+            'Country': ["field", "Country"],
+            'Area_HA': ["field", "Area_Ha"],
+            'National_Legal_Term': ["field", "Nat_Leg_Te"],
+            'Legal_Recognition': ["field", "Leg_Rec"],
+            }
+        }
+
+  NZL_Land_Rights = {
+        'location': "server",
+        'full_path': "D:\\GIS Data\\NZL\\New Zealand Land Rights\\NZL_Land_Rights.shp",
+        'where_clause': "",
         'transformation': None,
         'fields': {
-            'country': ["value", "LBR"],
-            'year': ["value", "2014"],
-            'type': ["field", "PERMIT_NUM"],
-            'name': ["field", "NAME"],
-            'company': ["field", "COMPANY"],
-            'group_company': ["field", "GROUP_"],
-            'group_country': ["field", "NAT_ORIGIN"],
-            'province': None,
-            'status': ["field", "Status"],
-            'area_ha': ["field", "AREA_HA"],
-            'source': ["value", "Global Witness 2014"]
+            'Name': ["field", "Name"],
+            'Country': ["field", "Country"],
+            'Area_HA': ["field", "Area_ha"],
+            'National_Legal_Term': ["field", "National_L"],
+            'Legal_Recognition': ["field", "Legal_Reco"],
+            'Category': ["value", "Land Rights"],
+            }
         }
-    }
+
+  Brazil_Final = {
+        'location': "server",
+        'full_path': "D:\\GIS Data\\BRA\\Brazil Land Rights\\Brazil_Final\\Brazil_Final.shp",
+        'where_clause': "",
+        'transformation': None,
+        'fields': {
+            'Name': ["field", "Name"],
+            'Country': ["field", "Country"],
+            'Area_HA': ["field", "Area_ha"],
+            'National_Legal_Term': ["field", "Nat_Leg_Te"],
+            'Legal_Recognition': ["field", "Leg_Rec"],
+            'Category': ["value", "Land Rights"],
+            }
+        }
   
+  Comarcas_Final = {
+        'location': "server",
+        'full_path': "D:\\GIS Data\\PAN\\Panama Land Rights\\Panama_Land_Rights\\Comarcas_Final.shp",
+        'where_clause': "",
+        'transformation': None,
+        'fields': {
+            'Name': ["field", "Name"],
+            'Country': ["field", "Country"],
+            'Area_HA': ["field", "Area_Ha"],
+            'National_Legal_Term': ["field", "Nat_Leg_Te"],
+            'Legal_Recognition': ["field", "Leg_Rec"],
+            'Category': ["value", "Land Rights"],
+            }
+        }
 
-
-    #return layer list
-    return [can_people]
+  Australia_Inalienable_Land_Rights = {
+        'location': "server",
+        'full_path': "D:\\GIS Data\\AUS\\Land Rights\\Australia_Inalienable_Land_Rights\\Australia_Inalienable_Land_Rights.shp",
+        'where_clause': "",
+        'transformation': None,
+        'fields': {
+            'Name': ["field", "Name"],
+            'Country': ["field", "Country"],
+            'Area_HA': ["field", "Area_Ofcl"],
+            'National_Legal_Term': ["field", "Category"],
+            'Legal_Recognition': ["field", "Reco"],
+            'Category': ["value", "Land Rights"],
+            }
+        }
+  
+      #return layer list
+    return [Canada_Land_rights, NZL_Land_Rights, Brazil_Final, Comarcas_Final, Australia_Inalienable_Land_Rights]
