e6037f10b7a7553f7e8fb4de31d2a3880d8aef6e
astrong19 <astrong@wri.org>
2015-10-14 09:46:13 -0400

    Edits to the metadata script
    fixed:
    - comma separated tags
    - translated title/overview
    - extent and update description

--- metadata_to_xml.py
+++ metadata_to_xml.py
@@ -30,7 +30,7 @@ function = wks.cell(cell.row, 5).value
 overview = wks.cell(cell.row, 6).value
 translated_overview = wks.cell(cell.row, 7).value
 category = wks.cell(cell.row, 10).value
-tags = wks.cell(cell.row, 11).value
+tags = wks.cell(cell.row, 11).value.split(", ")
 geographic_coverage = wks.cell(cell.row, 12).value
 date_of_content = wks.cell(cell.row, 13).value
 update_frequency = wks.cell(cell.row, 17).value 
@@ -47,15 +47,24 @@ metadata = md.MetadataEditor(filepath)
 metadata.title.set(title)
 metadata.abstract.set(overview)
 metadata.purpose.set(function)
-metadata.tags.set(tags)
-metadata.place_keywords.set(geographic_coverage)
+metadata.extent_description.set(geographic_coverage)
 metadata.temporal_extent_description.set(date_of_content)
-metadata.update_frequency.set(update_frequency)
+metadata.update_frequency_description.set(update_frequency)
 metadata.credits.set(credits)
 metadata.citation.set(citation)
 metadata.limitation.set(cautions)
 metadata.source.set(source)
 metadata.finish()
+
+#remove then add tags 
+metadata.tags.removeall()
+metadata.tags.add(tags)
+
+#add translated title and overview
+metadata.locales["french"].add()
+metadata.locales["french"].title.set(translated_title)
+metadata.locales["french"].abstract.set(translated_overview)
+
 print "metadata written from spreadsheet to xml"
 
 
