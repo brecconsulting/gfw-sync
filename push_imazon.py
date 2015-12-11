6940878436bc37bd52020403eb052b7074038907
Thomas Maschler <tmaschler@wri.org>
2015-12-10 19:31:23 -0500

    push imazon

--- push_imazon.py
+++ push_imazon.py
@@ -14,7 +14,17 @@ def get_auth_key():
 def cartodb_sql(sql):
     key = get_auth_key()
     return urllib.urlopen("http://wri-01.cartodb.com:80/api/v2/sql?api_key=%s&q=%s" % (key, sql))
+
     
+def cartodb_push(file_name):
+    key = get_auth_key()
+    subprocess.call([r'C:\Program Files\GDAL\ogr2ogr.exe',
+                    '--config', 'CARTODB_API_KEY', key,
+                    '-append', '-progress', '-skipfailures',
+                    '-t_srs', 'EPSG:4326',
+                    '-f', 'CartoDB',
+                    'CartoDB:wri-01', file_name])    
+
 
 def push_imazon():
     table = 'imazon_sad_copy2'
@@ -27,13 +37,7 @@ def push_imazon():
     cartodb_sql(sql)
 
     print "append"
-    key = get_auth_key()
-    subprocess.call([r'C:\Program Files\GDAL\ogr2ogr.exe',
-                    '--config', 'CARTODB_API_KEY', key,
-                     '-append', '-progress', '-skipfailures',
-                    '-t_srs', 'EPSG:4326',
-                    '-f', 'CartoDB',
-                    'CartoDB:wri-01', shp])
+    cartodb_push(shp)
     
     print "repair geometry"
     sql = 'UPDATE %s SET the_geom = ST_MakeValid(the_geom), the_geom_webmercator = ST_MakeValid(the_geom_webmercator) WHERE ST_IsValid(the_geom) = false' % table
