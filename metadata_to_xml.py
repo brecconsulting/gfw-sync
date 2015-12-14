24757f9c4837ec5f7de959e5894d5baad22aceac
astrong19 <astrong@wri.org>
2015-12-14 11:43:10 -0500

    revert

--- metadata_to_xml.py
+++ metadata_to_xml.py
@@ -10,10 +10,7 @@ script, filename, filepath = argv
 #specify oauth2client credentials 
 json_key = json.load(open('metadata_spreadsheet.json'))
 scope = ['https://spreadsheets.google.com/feeds']
-client_email = json_key['client_id']
-private_key = json_key['client_secret'].encode()
-credentials = SignedJwtAssertionCredentials(client_email, private_key, scope)
-#credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
 
 #authorize oauth2client credentials 
 gc = gspread.authorize(credentials)
