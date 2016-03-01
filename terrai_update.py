dfb14bbfbb7da5c5d877791d3f7821a5ea312271
astrong19 <astrong@wri.org>
2016-03-01 17:31:45 -0500

    Finish Terra I Script

--- terrai_update.py
+++ terrai_update.py
@@ -1,6 +1,3 @@
-''' author: Sam Gibbes and Asa Strong
-date: 2/2016
-'''
 import arcpy
 import datetime, time
 import urllib
@@ -46,60 +43,62 @@ def calculate_date():
             date_formatted= (year_format+days).strftime('%m/%d/%Y')
             row[1]=date_formatted
             cursor.updateRow(row)
+            export_shp()
             print "dates calculated"
 
-#call function 1 through 3
-download_data()
+#Convert File to Points and add to S3
+def export_shp():
+    print "sending points to S3"
+    input = "R://latin_decrease_current.tif"
+    output = "F://forest_change//terra_i_alerts//terra_i.shp"
+    arcpy.RasterToPoint_conversion(input, output, "date")
+    print "points uploaded to S3"
 
-#optional function to Reproject the data to equal area in the R:/analysis folder
-'''def reproject_data():
-    print "reprojecting data"
-    input = "R:\\latin_decrease_current.tif"
-    output = "R:\\analysis\\terra_i.tif"
-    prj = "R:\\analysis\\terra_i\\prj.adf"
-    arcpy.ProjectRaster_management(input, output, prj)
-    print "Terra_I Reproject to R:/analysis folder"'''
+#Zip file in S3
+def gen_paths(shp):
+    basepath, fname = os.path.split(shp)
+    base_fname = os.path.splitext(fname)[0]
 
-#Create zipped, unzipped, and archived raster in s3 for visualization.
+    return basepath, fname, base_fname
 
-def zip_raster(rst, dst):
-    basepath, fname, base_fname = gen_paths(rst)
-    # zip_name = timestamp + "_"+ base_fname + ".zip"
+
+def add_to_zip(fname, zf):
+
+    bname = os.path.basename(fname)
+    ending = os.path.splitext(bname)[1]
+    if not ending ==  ".lock" and not ending == ".zip" :
+        #print 'Writing %s to archive' % ending
+        # flatten zipfile
+        zf.write(fname, bname)
+
+    return
+
+
+def zip_shapefile(shp, dst):
+
+    basepath, fname, base_fname = gen_paths(shp)
+
     zip_name = base_fname + ".zip"
+
     zip_path = os.path.join(dst, zip_name)
     zf = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED, allowZip64=True)
 
-    bname = os.path.basename(rst)
-    if (base_fname in bname) and (bname != zip_name):
-        add_to_zip(rst, zf)
+    search = os.path.join(basepath, "*.*")
+    files = glob.glob(search)
+    for f in files:
+        bname = os.path.basename(f)
+        if (base_fname in bname) and (bname != zip_name):
+            add_to_zip(f, zf)
+
     zf.close()
 
-destination_latest_raster = r'F:\forest_change\terra_i_alerts\blank'
-zip_folder = r'F:\forest_change\terra_i_alerts\zip'
-archive_directory = r'F:\forest_change\terra_i_alerts\archive'
+    #print '\nZip archive complete:\n%s' % dst
 
-timestamp =  date.fromtimestamp(time.time()).strftime("%m%d%y")
-basename = "latest_raster.tif"
+    return zip_name
 
+shp = "F://forest_change//terra_i_alerts//terra_i.shp"
+dst = "F://forest_change//terra_i_alerts//zip"
 
-latest_raster = os.path.join(destination_latest_raster,basename)
-existing = glob.glob(os.path.join(destination_latest_raster,basename + "*"))
-print existing
-if len(existing) == 1:
-    for i in existing:
-##        print "archiving old raster"
-        shutil.copy(i,os.path.join(archive_directory,timestamp+"_"+basename))
-##        print "deleting old raster"
-        os.remove(i)
-if len(existing) > 1:
-    print "more than 1 raster exists, not sure which to archive, fail"
-    quit()
-##print "delete old zip"
-zips = glob.glob(zip_folder+"\\"+"*")
-if len(zips)>0:
-    for z in zips:
-        os.remove(z)
-##print "downloading latest raster"
-terrai_file = "R://latin_decrease_current.tif"
-##print "zip latest raster"
-zip_raster(latest_raster,zip_folder)
+#call functions
+download_data()
+zip_shapefile(shp, dst)
